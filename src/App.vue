d4036f8f2a1001fd27f95f534f7657f899c63aac
catsblack773 <47641520+catsblack773@users.noreply.github.com>
2021-11-29 11:07:21 +0400

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="app">
+    <h />
+    <div class="content">
+      <router-view />
+    </div>
+    <f />
+  </div>
+</template>
+
+<script>
+import h from './components/Header';
+import f from './components/Footer';
+
+export default {
+  name: 'app',
+  data () {
+    return {}
+  },
+  components: {
+    h,
+    f
+  }
+}
+
+</script>
+
+<style>
+
+</style>
