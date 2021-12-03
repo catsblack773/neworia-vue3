82adb4ac94d59bc0c73ce1ff625817d25d2fc7bd
azalalov <azalalov@sutochno.ru>
2021-12-03 12:33:00 +0400

    axios install

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,5 +1,17 @@
 <template>
   <div>
-  
+    <h3>count: {{ count }}</h3>
+    <input type="text" v-model="count">
   </div>
 </template>
+
+<script>
+
+export default {
+  data () {
+    return {
+      count: 0
+    }
+  }
+}
+</script>
